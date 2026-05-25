431799b9f67da21f298c548d0c4ef89ce53b91c1
IllusiveAldebaran <franciscofabiangutierrez@gmail.com>
2026-05-25 07:24:29 -0700

    Batched diagonal golden + flags cpu align
    Essentially just maps diagonal sequences into a single buffer
    Added -d and -b flags to output this.

--- align_cpu.cpp
+++ align_cpu.cpp
@@ -3,9 +3,11 @@
  * Sequence parser from https://github.com/IllusiveAldebaran/nussinov-gpu
  *
  * Reference Sequence Alignment
+ * Runs CPU alignment in typical Smith-Waterman
+ * Permutations (diagonal and batching) are only parsed into different buffers after
  *
  * Date of Creation: 3/29/26
- * Date Last Modified: 3/29/26
+ * Date Last Modified: 4/24/26
  */
 
 #include <cstdlib>
@@ -19,16 +21,16 @@
 #include <vector>
 #include <cctype>
 
+#include <filesystem>
+
+
 #include "utils/seq_utils.h"
 #include "utils/parser.hpp"
 
 int main(int argc, char * const argv[]) {
-  if (argc != 2) {
-      fprintf(stderr, "Usage: %s <file.fa|file.fa.gz>\n", argv[0]);
-      return 1;
-  }
+  struct args myargs = parse_args(argc, (const char **)argv);
 
-  auto pairs = load_pairs(argv[1]);
+  auto pairs = load_pairs(myargs.fasta.c_str());
 
   if (pairs.empty()) {
     fprintf(stderr, "No alignment pairs loaded\n");
@@ -46,33 +48,85 @@ int main(int argc, char * const argv[]) {
       return 1;
     }
   }
+  
+  // we can do this because we checked all ref_len are the same size
+  // If we want to use different cols then we'll have to check for a max ref_len
+  // +1 because we count the padding
+  uint32_t batchedDPCols = pairs[0].ref_len + 1;
+  uint32_t batchedDPRows = pairs[0].query_len + 1;
+
+  // we reuse refLen and qryLen batching. Perhaps a misnomer, but who cares.
+  // We modify our string to have 0s before every sequence
+  SeqPair batchedPairs{}; // massive pairs, initialize struct to blanks
+  if(myargs.batch) {
+    // tries to get filename as name of the batched sequence
+    auto p = std::filesystem::path(myargs.fasta);
+    if (p.extension() == ".gz") p = p.stem();
+    std::string stem = p.stem().string();
+
+    // drops the names of the pairs in favor of just calling it by the file name
+    batchedPairs.name = stem;
+
+    for (auto& p : pairs) {
+      batchedPairs.ref       += "0" + p.ref;
+      batchedPairs.query     += "0" + p.query;
+      batchedPairs.ref_len   += p.ref_len + 1; // we add one as we are now counting padding
+      batchedPairs.query_len += p.query_len + 1;
+    }
+  }
 
   uint16_t* DP  = (uint16_t*)calloc((refLen+1)*(qryLen+1), sizeof(uint16_t));
-  uint16_t* DPD = (uint16_t*)calloc((refLen+1+qryLen)*(qryLen+1), sizeof(uint16_t));
+  uint16_t* DPD;
+  uint16_t* DPDB;
+  // we use this buffer to generate DPDB
+  if(myargs.diagonal || myargs.batch) {
+    DPD = (uint16_t*)calloc((refLen+1+qryLen)*(qryLen+1), sizeof(uint16_t));
+  }
+  if(myargs.batch) {
+    // we generate a gigantic sequence as a conctenation of all sequences
+    DPDB = (uint16_t*)calloc((batchedPairs.query_len + batchedDPCols - 1)*(batchedDPCols), sizeof(uint16_t));
+  }
 
   // separate from alignment pair struct in case this sequence is transformed or changed
   uint8_t* refSeq = (uint8_t*)calloc(refLen, sizeof(uint8_t));
   uint8_t* qrySeq = (uint8_t*)calloc(qryLen, sizeof(uint8_t));
 
+  {
+  uint32_t seqIdx = 0; // pair counter, only used for batching placement
   for (auto& p : pairs) {
-    printf("Alignment Pair: Name:%s Ref:%s Qry:%s Sizes:%zux%zu\n",
-           p.name.c_str(),
-           p.ref.c_str(),
-           p.query.c_str(),
-           p.ref.length(),
-           p.query.length());
+
+    printf("%s", p.toString().c_str());
 
     memcpy(refSeq, p.ref.c_str(), refLen);
     memcpy(qrySeq, p.query.c_str(), qryLen);
 
     fillDPSmithWaterman(refSeq, refLen, qrySeq, qryLen, DP);
-    fillDPtoDPD(DP, DPD, refLen+1, qryLen+1);
-    showDP(refSeq, refLen, qrySeq, qryLen, DP);
-    showDP(refSeq, refLen, qrySeq, qryLen, DPD, true);
+    if(!myargs.diagonal && !myargs.batch) {
+      showDP(refSeq, refLen, qrySeq, qryLen, DP);
+    } else {
+      fillDPtoDPD(DP, DPD, refLen+1, qryLen+1);
+      // only show if we're not batching, else just collect it into the batch buffer
+      
+      if(!myargs.batch) {
+        showDP(refSeq, refLen, qrySeq, qryLen, DPD, true);
+      } else {
+        // fill up the batch buffer
+        fillDPDtoDPDB(DPD, DPDB, refLen+1, qryLen+1, seqIdx);
+        seqIdx += qryLen+1; // +1 because extra 0 padding
+      }
+    }
+  }
+  }
+
+  if (myargs.batch) {
+    printf("%s\n", batchedPairs.toString().c_str());
+    showDPDB((uint8_t*)batchedPairs.ref.c_str(), batchedPairs.ref_len, (uint8_t*)batchedPairs.query.c_str(), batchedPairs.query_len, batchedDPRows, batchedDPCols, DPDB);
   }
 
   free(refSeq);
   free(qrySeq);
   free(DP);
-  free(DPD);
+
+  if(myargs.diagonal || myargs.batch) free(DPD);
+  if(myargs.batch) free(DPDB);
 }
--- utils/parser.hpp
+++ utils/parser.hpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 
 #include "seq_utils.h"
+#include "cxxopts.hpp"
 
 using namespace klibpp;
 
@@ -23,6 +24,48 @@ std::vector<SeqPair> load_pairs(const char* path) {
         });
     }
     return pairs;
+}
+
+
+struct args {
+  bool diagonal;
+  bool batch;
+  std::string fasta;
+};
+
+struct args parse_args(int argc, const char *argv[]) {
+  cxxopts::Options options("align_cpu", "CPU Smith-Waterman alignment");
+  options.add_options()
+    ("d,diagonal", "Run in diagonal mode",  cxxopts::value<bool>()->default_value("false"))
+    ("b,batch",    "Run in batch mode",     cxxopts::value<bool>()->default_value("false"))
+    ("f,fasta",    "Input FASTA file",      cxxopts::value<std::string>())
+    ("h,help",     "Print usage");
+  options.parse_positional({"fasta"});
+
+  cxxopts::ParseResult vm;
+  try {
+    vm = options.parse(argc, argv);
+  } catch (const cxxopts::exceptions::parsing &e) {
+    fprintf(stderr, "Error: %s\n%s\n", e.what(), options.help().c_str());
+    exit(1);
+  }
+
+  if (vm.count("help") || !vm.count("fasta")) {
+    fprintf(stderr, "%s\n", options.help().c_str());
+    exit(vm.count("help") ? 0 : 1);
+  }
+
+  struct args myargs;
+  myargs.diagonal = vm["diagonal"].as<bool>();
+  myargs.batch    = vm["batch"].as<bool>();
+  myargs.fasta    = vm["fasta"].as<std::string>();
+
+  if (myargs.diagonal && myargs.batch) {
+    fprintf(stderr, "Error: -d and -b are mutually exclusive\n");
+    exit(1);
+  }
+
+  return myargs;
 }
 
 
